9fe284dc0201bbe92fb3507b016ec1e49b9070b5
Sergio Vargas <svargas.ext.uber@gmail.com>
2023-07-29 20:53:22 -0500

    form ended

--- practices/contact_form/index.php
+++ practices/contact_form/index.php
@@ -1,4 +1,70 @@
-<?php 
+<?php
+
+    // 1- The first thing we will do is to create empty variables
+    // 2- Check if the form was submitted
+    // 3- assinged a function to each variable with the data enter by the user
+    // 4- Create the function that will pass through all the functions to clean and validate the data
+
+    $name = $email = $message = "";
+    $name_error = $email_error = $message_error = "";
+    
+    if (isset($_POST['submit'])) {
+        
+        if (!empty($_POST['name'])) {
+            $name = test_validation($_POST['name']);
+            if (!preg_match("/^[a-zA-Z' ]*$/", $name)) {
+                $name_error = "Invalid name: Only letters and white space allowed";
+            }
+        } else {
+            $name_error = "name mandatory";
+        }
+
+        if (!empty($_POST['email'])) {
+            $email = test_validation($_POST['email']);
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                 $email_error = "Invalid email format";
+            }
+        } else {
+            $email_error = "email mandatory";
+            }
+        if (!empty($_POST['message'])) {
+            $message = test_validation($_POST['message']);
+        } else {
+            $message_error = "message mandatory";
+        }
+    }
+
+
+
+    function test_validation($data) {
+        $data = trim($data);
+        $data = htmlspecialchars($data);
+        $data = stripslashes($data);
+
+        return $data;
+    } 
+
+
+    // $errors = '';
+    // $sent = '';
+    // if (isset($_POST['submit'])) {
+        
+    //         $name = $_POST['name'];
+    //         $email = $_POST['email'];
+    //         $message = $_POST['message'];
+    //         echo $name;
+    //         echo $email;
+    //         echo $message; 
+
+    //     if (!empty($name)) {
+    //         $name = trim($name);
+    //         $name = filter_var($name, FILTER_SANITIZE_STRING);
+    //         // $sent = 'Succesffully sent';
+    //     } else {
+    //         $errors .= 'Please add a name <br />'; 
+    //     }
+    // }
+
     require 'index.view.php';
     
 ?>
--- practices/contact_form/index.view.php
+++ practices/contact_form/index.view.php
@@ -8,17 +8,32 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+
 </head>
 <body>
     <main>
-        <div class="wrap_container">
-            <input type="text" name="name" id="name" placeholder="Write your name">
+        <div>
+            <form class="wrap_container" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
+                <input type="text" name="name" id="name" placeholder="Write your name" value="<?php echo $name;?>">
+                <span class="alert"><?php echo $name_error;?></span>
+                <input type="email" name="email" id="email" placeholder="Write your email" value="<?php echo $email;?>">
+                <span class="alert"><?php echo $email_error;?></span>
+                <textarea name="message" id="message" cols="10" rows="5" placeholder="Write a message" value="<?php echo $message;?>"></textarea>
+                <span class="alert"><?php echo $message_error;?></span>
 
-            <input type="email" name="name" id="name" placeholder="Write your email">
+<!-- 
+                <?php if (!empty($errors)): ?>
+                    <div class="alert error">
+                        <?php echo $errors; ?>
+                    </div>
+                <?php elseif($sent): ?>
+                    <div class="alert success">
+                        <p>Sent correctly</p>
+                    </div>
+                <?php endif ?>  -->
 
-            <textarea name="message" id="message" cols="10" rows="5" placeholder="Write a message"></textarea>
-
-            <input type="submit" value="Send email">
+                <input type="submit" name="submit" value="Send email">
+            </form>
         </div>
     </main>
 </body>
