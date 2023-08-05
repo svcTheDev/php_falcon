db143b6e35e037b8fff16002469c94a75e7897ee
sergio <svargas.ext.uber@gmail.com>
2023-08-05 08:15:40 -0500

    Start POO part 2 this pc

--- POO/classes.php
+++ POO/classes.php
@@ -7,6 +7,8 @@
         $this->name = $name  . ' ';
     }
 
+    reconocimiento del hardware y los detalles de cada tecnología, práctica de desmontaje y montaje de equipos (escritorio, todo en uno y portátiles), reparaciones o cambios comunes, tipos de arquitecturas computadores escritorio, todo en uno y portátiles, compatibilidad de perifericos y nuevas tecnologias, prácticas de red y conexiones eléctricas (dc y ac) y multímetro, mantenimiento, cambio crema y limpieza, instalación y mantenimiento software, recuperar datos, limpieza de virus, programas para optimizar el windows, sitios de referencia para estar actualizado
+
     function get_name() {
         return $this->name;
     }
